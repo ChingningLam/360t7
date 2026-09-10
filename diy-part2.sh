e285620f38dbca8474963e9075c92e84b36bf7e9
7n <63094092+ChingningLam@users.noreply.github.com>
2026-09-10 18:02:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -7,5 +7,3 @@ sed -i 's/192.168.1.1/192.168.3.1/g' ./package/base-files/files/bin/config_gener
 sed -i 's/hostname="ImmortalWrt"/hostname="360T7"/g' ./package/base-files/files/bin/config_generate
 # 最底部
 sed -i 's/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION="ImmortalWrt By ChingningLam $(date +%Y%m%d)"/g' ./package/base-files/files/etc/openwrt_release
-# 获取ua3f
-git clone https://github.com/SunBK201/UA3F.git package/UA3F
